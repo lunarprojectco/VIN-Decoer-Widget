04ae174fa97830465ea166417f1cdf92d272270b
lunarprojectco <128015691+lunarprojectco@users.noreply.github.com>
2023-03-15 22:57:08 -0600

    Update vin-decoder-widget.js

--- vin-decoder-widget.js
+++ vin-decoder-widget.js
@@ -1,83 +1,88 @@
-// Variables to store the DOM elements
-const video = document.getElementById('vin-decoder-widget-video');
-const canvas = document.getElementById('vin-decoder-widget-canvas');
-const scanBtn = document.getElementById('vin-decoder-widget-scan-btn');
-const resultsContainer = document.getElementById('vin-decoder-widget-results');
+(function ($) {
+  // Define constants
+  const CAR_MD_API_BASE_URL = 'https://api.carmd.com/v3.0';
+  const PARTNER_TOKEN = 'c66918d63fa043e4afb3e8fa7bf37951';
+  const AUTHORIZATION_KEY = 'Basic OWU1NWM3OGEtZGRhMC00NmRhLWI0YjYtM2Y5ODA1YTBhZDYz';
 
-// Variables to store the ML Kit and CarMD API objects
-let vision;
-let carmd;
-
-// Variables to store the API keys
-const mlKitApiKey = 'AIzaSyC3iRR3sSC07cXfIWQjO0gVze6AtP18tLw';
-const carmdApiKey = 'c66918d63fa043e4afb3e8fa7bf37951';
-const carmdAuthKey = 'Basic OWU1NWM3OGEtZGRhMC00NmRhLWI0YjYtM2Y5ODA1YTBhZDYz';
-
-// Function to initialize the ML Kit and CarMD API objects
-async function initialize() {
-    // Initialize the ML Kit Vision object
-    await firebase.initializeApp({
-        apiKey: mlKitApiKey,
-    });
-
-    vision = await firebase
-        .app()
-        .functions('us-central1')
-        .httpsCallable('processImage');
-
-    // Initialize the CarMD API object
-    carmd = axios.create({
-        baseURL: 'https://api.carmd.com/v3.0',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Partner-Token': carmdApiKey,
-            'Authorization': carmdAuthKey,
+  // Define helper functions
+  function decodeVIN(vin) {
+    return new Promise(function (resolve, reject) {
+      const requestUrl = CAR_MD_API_BASE_URL + '/decode?vin=' + encodeURIComponent(vin);
+      $.ajax({
+        url: requestUrl,
+        type: 'GET',
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('partner-token', PARTNER_TOKEN);
+          xhr.setRequestHeader('authorization', AUTHORIZATION_KEY);
         },
+        success: function (data) {
+          resolve(data);
+        },
+        error: function (xhr, status, error) {
+          reject(xhr.responseText);
+        }
+      });
     });
-}
+  }
 
-// Function to decode the VIN number
-async function decodeVinNumber() {
-    // Get the current video stream
-    const stream = video.srcObject;
+  function displayVINData(data) {
+    $('#vin-widget-output').html(
+      '<ul>' +
+      '<li><strong>Year:</strong> ' + data.year + '</li>' +
+      '<li><strong>Make:</strong> ' + data.make + '</li>' +
+      '<li><strong>Model:</strong> ' + data.model + '</li>' +
+      '<li><strong>Trim:</strong> ' + data.trim + '</li>' +
+      '<li><strong>Engine:</strong> ' + data.engine + '</li>' +
+      '<li><strong>Drive Type:</strong> ' + data.driveType + '</li>' +
+      '<li><strong>Vehicle Type:</strong> ' + data.vehicleType + '</li>' +
+      '<li><strong>Body Type:</strong> ' + data.bodyType + '</li>' +
+      '<li><strong>Fuel Type:</strong> ' + data.fuelType + '</li>' +
+      '</ul>'
+    );
+  }
 
-    // Stop the video stream
-    video.srcObject = null;
-    stream.getVideoTracks()[0].stop();
+  // Define click handler for scan button
+  $('#vin-widget-scan-button').click(function () {
+    // Check if camera is supported by device
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      alert('Sorry, your device does not support camera scanning.');
+      return;
+    }
 
-    // Set the canvas dimensions to match the video dimensions
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    // Get reference to video element
+    const videoElement = $('#vin-widget-video').get(0);
 
-    // Draw the current video frame to the canvas
-    const context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    // Request permission to access camera
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        // Start playing video stream in video element
+        videoElement.srcObject = stream;
+        videoElement.play();
+      })
+      .catch(function (error) {
+        alert('Sorry, there was an error accessing the camera: ' + error.message);
+      });
 
-    // Convert the canvas image to a base64-encoded string
-    const image = canvas.toDataURL('image/jpeg');
+    // Define function to stop video stream
+    function stopVideoStream() {
+      videoElement.pause();
+      videoElement.srcObject = null;
+      stream.getTracks().forEach(function (track) {
+        track.stop();
+      });
+    }
 
-    // Use ML Kit Vision to process the image and extract the VIN number
-    const response = await vision({
-        image: image,
-        features: ['TEXT_DETECTION'],
-    });
+    // Define function to process scanned VIN
+    function processScannedVIN(vin) {
+      decodeVIN(vin)
+        .then(function (data) {
+          displayVINData(data);
+          stopVideoStream();
+        })
+        .catch(function (error) {
+          alert('Sorry, there was an error decoding the VIN: ' + error);
+          stopVideoStream();
+        });
+    }
 
-    const text = response.data.responses[0].fullTextAnnotation.text;
-    const vin = text.match(/[A-HJ-NPR-Z0-9]{17}/g)[0];
-
-    // Use the CarMD API to get the vehicle details for the extracted VIN number
-    const vehicleDetails = await carmd.get(`/vehicle?vin=${vin}`);
-
-    // Display the vehicle details in the results container
-    resultsContainer.innerHTML = `
-        <h2>Vehicle Details:</h2>
-        <p><strong>VIN:</strong> ${vin}</p>
-        <p><strong>Make:</strong> ${vehicleDetails.data.data.make}</p>
-        <p><strong>Model:</strong> ${vehicleDetails.data.data.model}</p>
-        <p><strong>Year:</strong> ${vehicleDetails.data.data.year}</p>
-        <p><strong>Engine:</strong> ${vehicleDetails.data.data.engine}</p>
-        <p><strong>Trim:</strong> ${vehicleDetails.data.data.trim}</p>
-    `;
-}
-
+    // Set up barcode scanner
